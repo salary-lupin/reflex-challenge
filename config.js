ed038c1838962da6bf8408cf617c5ab138d57cf4
salary-lupin <salary-lupin@users.noreply.github.com>
2026-05-07 06:32:30 +0000

    deploy: 87fca2314eeaeef8dd59f57fe7300957ef2137f6

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCKlEO8xvJ_zzZU9VpEGAt0CspHjtVoU3U",
+  authDomain: "ranktest-d8902.firebaseapp.com",
+  databaseURL: "https://ranktest-d8902-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ranktest-d8902",
+  storageBucket: "ranktest-d8902.firebasestorage.app",
+  messagingSenderId: "451797429217",
+  appId: "1:451797429217:web:dc5d6d4dab66b523b440a0"
+};
